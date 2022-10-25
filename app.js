594b035b573304cf75b287aab8951383875d4f2a
Agent2307 <rustin.neo@gmail.com>
2022-10-25 15:34:11 +0800

    Update app.js

--- app.js
+++ app.js
@@ -3,13 +3,13 @@
 // app.js is the main entry point for your 8th Wall app. Code here will execute after head.html
 // is loaded, and before body.html is loaded.
 import './main.css'
-import {tapPlaceCursorComponent} from './components/tap-place-cursor'
+import {tapPlaceCursorComponent} from './tap-place-cursor'
 
 // Register custom A-Frame components in app.js before the scene in body.html has loaded.
 AFRAME.registerComponent('tap-place-cursor', tapPlaceCursorComponent)
 
-import {cubeMapRealtimeComponent} from './components/cubemap-realtime'
+import {cubeMapRealtimeComponent} from './cubemap-realtime'
 AFRAME.registerComponent('cubemap-realtime', cubeMapRealtimeComponent)
 
-import {cubeEnvMapComponent} from './components/cubemap-static'
+import {cubeEnvMapComponent} from './cubemap-static'
 AFRAME.registerComponent('cubemap-static', cubeEnvMapComponent)
